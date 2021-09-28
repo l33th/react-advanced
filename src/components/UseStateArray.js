1966f28a405e1541d603d147b46ef537573d0695
l33th <l33th@protonmail.ch>
2021-09-28 20:32:47 +0200

    Remove Single Item Buttons

--- src/components/UseStateArray.js
+++ src/components/UseStateArray.js
@@ -16,7 +16,7 @@ const UseStateArray = () => {
 				return (
 					<div key={id} className='item'>
 						<h4>{name}</h4>
-            <button onClick={() => removeItem(id)}>remove item</button>
+            <button onClick={() => removeItem(id)}>remove</button>
 					</div>
 				);
 			})}

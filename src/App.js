d3af44ce2ce8ef4ab39e60a137313c474c32f52d
l33th <l33th@protonmail.ch>
2021-09-28 19:04:59 +0200

    Added Click Function

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ErrorExample from './components/error-example';
+
+function App() {
+  return (
+    <div className='container'>
+      <h2>Advanced Tutorial</h2>
+      <ErrorExample />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/error-example.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ErrorExample = () => {
+	let title = "Error React";
+  
+  const handleClick = () => {
+    console.log(title);
+  }
+
+	return (
+		<>
+			<h2>{title}</h2>
+      <button type='button' className='btn' onClick={handleClick}>
+        change title
+      </button>
+		</>
+	);
+};
+
+export default ErrorExample;

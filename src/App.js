24f8e3296e2a66935cb925c23dbbf2d956bce455
l33th <l33th@protonmail.ch>
2021-09-28 19:13:14 +0200

    Added useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ErrorExample from './components/error-example';
+import usingStateBasics from './components/usingStateBasics';
 
 function App() {
   return (
     <div className='container'>
       <h2>Advanced Tutorial</h2>
       <ErrorExample />
+      <usingStateBasics />
     </div>
   )
 }

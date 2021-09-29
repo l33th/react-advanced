de5702b1ca4a55e2eec2256103071c4e8d408984
l33th <l33th@protonmail.ch>
2021-09-29 09:52:39 +0200

    Display Data in Browser

--- src/components/UseStateObject.js
+++ src/components/UseStateObject.js
@@ -6,8 +6,14 @@ const UseStateObject = () => {
 		age: 33,
 		message: "Hello, World",
 	});
-	console.log(person);
-	return <h2>useState object example</h2>;
+	// console.log(person);
+	return (
+		<>
+			<h3>{person.name}</h3>
+			<h3>{person.age}</h3>
+			<h3>{person.message}</h3>
+		</>
+	);
 };
 
 export default UseStateObject;

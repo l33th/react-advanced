a3cccfc7f51e756dd59be1370f3ec0060e754764
l33th <l33th@protonmail.ch>
2021-09-29 09:45:31 +0200

    Console Logging Person Object

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import UseStateObject from './components/UseStateObject';
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced Tutorial</h2>
+      <h2>Advanced React</h2>
       <UseStateObject />
     </div>
   )
--- src/components/UseStateObject.js
+++ src/components/UseStateObject.js
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
 
 const UseStateObject = () => {
+	const [person, setPeople] = useState({
+		name: "Genthos",
+		age: 33,
+		message: "Hello, World",
+	});
+	console.log(person);
 	return <h2>useState object example</h2>;
 };
 

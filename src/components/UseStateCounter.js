1ac090551b9795d1d5543954e632372d09f6a434
l33th <l33th@protonmail.ch>
2021-09-29 10:38:14 +0200

    More Complex Counter

--- src/components/UseStateCounter.js
+++ src/components/UseStateCounter.js
@@ -5,9 +5,19 @@ const UseStateCounter = () => {
 	const reset = () => {
 		setValue(0);
 	};
+
+	const complexIncrease = () => {
+		// setValue(value + 1);
+		setTimeout(() => {
+			setValue((prevState) => {
+				return prevState + 1;
+			});
+		}, 2000);
+	};
+
 	return (
 		<>
-			<section style={{ margin: '4rem 0'}}>
+			<section style={{ margin: "4rem 0" }}>
 				<h2>regular counter</h2>
 				<h1>{value}</h1>
 				<button className='btn' onClick={() => setValue(value + 1)}>
@@ -16,7 +26,17 @@ const UseStateCounter = () => {
 				<button className='btn' onClick={() => setValue(value - 1)}>
 					decrease
 				</button>
-				<button className='btn'onClick={reset}>reset</button>
+				<button className='btn' onClick={reset}>
+					reset
+				</button>
+			</section>
+
+			<section style={{ margin: "4rem 0" }}>
+				<h2>more complex counter</h2>
+				<h1>{value}</h1>
+				<button className='btn' onClick={complexIncrease}>
+					increase later
+				</button>
 			</section>
 		</>
 	);

bf03085fe984fdc2064a51ab2d0b1b89bb48ff3a
l33th <l33th@protonmail.ch>
2021-10-05 07:00:53 +0200

    useEffect - update title if != 1

--- src/components/UseEffect.js
+++ src/components/UseEffect.js
@@ -2,18 +2,23 @@ import React, { useState, useEffect } from "react";
 
 // useEffect runs after every re-render by default
 const UseEffect = () => {
-    const [value, setValue] = useState(0);
+	const [value, setValue] = useState(0);
 	useEffect(() => {
-		console.log("call useEffect");
-        document.title = `New Messages(${value})`
+        // Will only update the title if the value is != 1
+		if (value >= 1) {
+			console.log("call useEffect");
+			document.title = `New Messages(${value})`;
+		}
 	});
 	console.log("render component");
 
 	return (
 		<div>
-            <h2>use effect</h2>
+			<h2>use effect</h2>
 			<h1>{value}</h1>
-            <button className='btn' onClick={() => setValue(value + 1)}>increase</button>
+			<button className='btn' onClick={() => setValue(value + 1)}>
+				increase
+			</button>
 		</div>
 	);
 };

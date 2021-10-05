e75331fddbb7cad1faffee963b67d3b89d5ca18e
l33th <l33th@protonmail.ch>
2021-10-05 06:53:40 +0200

    useEffect Basics - no prevState button

--- src/components/UseEffect.js
+++ src/components/UseEffect.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from "react";
 
 const UseEffect = () => {
+    const [value, setValue] = useState(0);
 	useEffect(() => {
 		console.log("call useEffect");
 	});
 	console.log("render component");
-    
+
 	return (
 		<div>
-			<h1>use effect</h1>
+            <h2>use effect</h2>
+			<h1>{value}</h1>
+            <button className='btn' onClick={() => setValue(value + 1)}>increase</button>
 		</div>
 	);
 };

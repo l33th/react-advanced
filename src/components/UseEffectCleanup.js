747b43428fd0801375ab0972332edae121bfe338
l33th <l33th@protonmail.ch>
2021-10-05 07:42:37 +0200

    useEffect Cleanup - Second Parameter

--- src/components/UseEffectCleanup.js
+++ src/components/UseEffectCleanup.js
@@ -9,8 +9,15 @@ const UseEffectCleanup = () => {
 	};
 
 	useEffect(() => {
+		console.log("useEffect");
 		window.addEventListener("resize", checkSize);
-	});
+		return () => {
+			console.log("cleanup");
+			window.removeEventListener("resize", checkSize);
+		};
+	}, []);
+	console.log("render");
+
 	return (
 		<>
 			<h1>window size</h1>

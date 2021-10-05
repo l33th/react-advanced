e15133e110b90ac8beee23d03f5aecbc7867a29b
l33th <l33th@protonmail.ch>
2021-10-05 07:33:30 +0200

    useEffect Cleanup - Window Size

--- src/components/UseEffectCleanup.js
+++ src/components/UseEffectCleanup.js
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
+import { useEffect } from "react/cjs/react.development";
 
 const UseEffectCleanup = () => {
 	const [size, setSize] = useState(window.innerWidth);
-	console.log(size);
 
+	const checkSize = () => {
+		setSize(window.innerWidth);
+	};
+
+	useEffect(() => {
+		window.addEventListener("resize", checkSize);
+	});
 	return (
-		<div>
-			<h1>use effect cleanup</h1>
-		</div>
+		<>
+			<h1>window size</h1>
+			<h2>{size} PX</h2>
+		</>
 	);
 };
 

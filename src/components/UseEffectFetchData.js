5d71a845e42678e7c8f250b1b336527f2efa197e
l33th <l33th@protonmail.ch>
2021-10-06 09:50:46 +0200

    Display Github Users

--- src/components/UseEffectFetchData.js
+++ src/components/UseEffectFetchData.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 
 const url = "https://api.github.com/users";
 
 const UseEffectFetchData = () => {
+	const [users, setUsers] = useState([]);
+
+	const getUsers = async () => {
+		const response = await fetch(url);
+		const users = await response.json();
+		setUsers(users);
+	};
+
+	useEffect(() => {
+		getUsers();
+	}, []);
+
 	return (
 		<div>
 			<h2>UseEffectFetchData</h2>
-			<h3>fetch data</h3>
-			<button className='btn'>click me</button>
+			<h3>github users</h3>
+			<ul className='users'>
+				{users.map((user) => {
+					const { id, login, avatar_url, html_url } = user;
+					return (
+						<li key={id}>
+							<img src={avatar_url} alt={login} />
+							<div>
+								<h4>{login}</h4>
+								<a href={html_url}>profile</a>
+							</div>
+						</li>
+					);
+				})}
+			</ul>
 		</div>
 	);
 };

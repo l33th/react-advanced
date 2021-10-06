6df966357238729dcdc958b195ae775e341aa1fa
l33th <l33th@protonmail.ch>
2021-10-06 11:40:21 +0200

    Show/Hide Window Width

--- src/components/ShowHide.js
+++ src/components/ShowHide.js
@@ -1,16 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const ShowHide = () => {
 	const [show, setShow] = useState(false);
-    
-    const Item = () => {
-        return (
-            <div style={{ marginTop: "2rem" }}>
-                <h1>window</h1>
-                <h2>size: </h2>
-            </div>
-        );
-    };
+
+	const Item = () => {
+		const [size, setSize] = useState(window.innerWidth);
+
+		const checkSize = () => {
+			setSize(window.innerWidth);
+		};
+
+		useEffect(() => {
+			window.addEventListener("resize", checkSize);
+		}, []);
+		return (
+			<div style={{ marginTop: "2rem" }}>
+				<h1>window</h1>
+				<h2>size: {size} PX</h2>
+			</div>
+		);
+	};
 
 	return (
 		<div>
@@ -18,7 +27,7 @@ const ShowHide = () => {
 			<button className='btn' onClick={() => setShow(!show)}>
 				show/hide
 			</button>
-            {show && <Item />}
+			{show && <Item />}
 		</div>
 	);
 };

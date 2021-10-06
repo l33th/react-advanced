5d436595c77168434dabb2a04f9cbf6491d93cf3
l33th <l33th@protonmail.ch>
2021-10-06 10:15:40 +0200

    Error

--- src/components/ConditionalRenderingMultipleReturns.js
+++ src/components/ConditionalRenderingMultipleReturns.js
@@ -2,15 +2,22 @@ import React, { useState } from "react";
 const url = "https://api.github.com/users";
 
 const ConditionalRenderingMultipleReturns = () => {
-	const [loading, setLoading] = useState(false);
+	const [isLoading, setIsLoading] = useState(true);
+	const [isError, setIsError] = useState(false);
+	const [user, setUser] = useState("default user");
 
-	if (loading) {
+	if (isLoading) {
 		return <h1>Loading...</h1>;
 	}
+	if (isError) {
+		return <h1>Error !!!</h1>;
+	}
+
 	return (
 		<div>
 			<h1>Conditional Rendering</h1>
 			<h2>Multiple Returns</h2>
+			<h1>{user}</h1>
 		</div>
 	);
 };

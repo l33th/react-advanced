1736ece8e714b4493b7d3d3bc9ecdc33fbde464d
l33th <l33th@protonmail.ch>
2021-10-07 06:43:58 +0200

    Added Form - Email

--- src/components/FormsControlledInputs.js
+++ src/components/FormsControlledInputs.js
@@ -9,6 +9,11 @@ const FormsControlledInputs = () => {
 						<label htmlFor='firstName'>Name : </label>
 						<input type='text' id='firstName' name='firstName' />
 					</div>
+
+					<div className='form-control'>
+						<label htmlFor='email'>Email : </label>
+						<input type='text' id='email' name='email' />
+					</div>
 				</form>
 			</article>
 		</>

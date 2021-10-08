c507a882558a08e8bf8ee20a9c63857424aeea49
l33th <l33th@protonmail.ch>
2021-10-08 07:47:02 +0200

    Added State

--- src/components/FormsControlledInputs.js
+++ src/components/FormsControlledInputs.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 const FormsControlledInputs = () => {
+	const [firstName, setFirstName] = useState("");
+	const [email, setEmail] = useState("");
+
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		console.log("hello world");
@@ -11,12 +14,22 @@ const FormsControlledInputs = () => {
 				<form className='form' onSubmit={handleSubmit}>
 					<div className='form-control'>
 						<label htmlFor='firstName'>Name : </label>
-						<input type='text' id='firstName' name='firstName' />
+						<input
+							type='text'
+							id='firstName'
+							name='firstName'
+							value={firstName}
+						/>
 					</div>
 
 					<div className='form-control'>
 						<label htmlFor='email'>Email : </label>
-						<input type='text' id='email' name='email' />
+						<input
+							type='text'
+							id='email'
+							name='email'
+							value={email}
+						/>
 					</div>
 					<button type='submit'>add person</button>
 				</form>

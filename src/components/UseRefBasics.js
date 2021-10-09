999aa46ef3dfa12665dec070700dd07992aa69f2
l33th <l33th@protonmail.ch>
2021-10-09 05:30:46 +0200

    useRef Form Submit

--- src/components/UseRefBasics.js
+++ src/components/UseRefBasics.js
@@ -4,6 +4,14 @@ const UseRefBasics = () => {
 	return (
 		<div>
 			<h2>use ref basics</h2>
+			<form className='form'>
+				<div>
+					<input type='text' />
+					<button className='btn' type='button'>
+						submit
+					</button>
+				</div>
+			</form>
 		</div>
 	);
 };

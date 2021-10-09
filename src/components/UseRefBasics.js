1d9e7476ae2ee26bc469defd94eb20fb75b81c2c
l33th <l33th@protonmail.ch>
2021-10-09 05:39:16 +0200

    using useRef

--- src/components/UseRefBasics.js
+++ src/components/UseRefBasics.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const UseRefBasics = () => {
+	const initialValue = null;
+	const handleSubmit = (e) => {
+		const refContainer = useRef(initialValue);
+		e.preventDefault();
+	};
+	console.log(refContainer);
+
 	return (
 		<div>
 			<h2>use ref basics</h2>
-			<form className='form'>
+			<form className='form' onSubmit={handleSubmit}>
 				<div>
-					<input type='text' />
+					<input type='text' ref={refContainer} />
 					<button className='btn' type='button'>
 						submit
 					</button>

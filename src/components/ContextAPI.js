7232f7d8063bc746be5d4b79921e942ee5757899
l33th <l33th@protonmail.ch>
2021-10-21 06:57:45 +0200

    Added useContext - PersonContext.Provider

--- src/components/ContextAPI.js
+++ src/components/ContextAPI.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useState, useContext } from "react";
+import { data } from "../data";
+
+const PersonContext = React.createContext();
 
 const ContextAPI = () => {
-    return (
-        <div>
-            <h1>Context API</h1>
-        </div>
-    )
-}
+	const [people, setPeople] = useState(data);
 
-export default ContextAPI
+	const removePerson = (id) => {
+		setPeople((people) => {
+			return people.filter((person) => person.id !== id);
+		});
+	};
+
+	return (
+		<PersonContext.Provider value='hello'>
+			<h3>Prop Drilling</h3>
+			<List people={people} removePerson={removePerson} />
+		</PersonContext.Provider>
+	);
+};
+
+const List = ({ people, removePerson }) => {
+	return (
+		<>
+			{people.map((person) => {
+				return (
+					<SinglePerson
+						key={person.id}
+						{...person}
+						removePerson={removePerson}
+					/>
+				);
+			})}
+		</>
+	);
+};
+
+const SinglePerson = ({ id, name, removePerson }) => {
+	return (
+		<div className='item'>
+			<h4>{name}</h4>
+			<button className='btn' onClick={() => removePerson(id)}>
+				remove
+			</button>
+		</div>
+	);
+};
+
+export default ContextAPI;

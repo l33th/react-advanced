4506f509a41c9e5cd577b66617f489d9f75df673
l33th <l33th@protonmail.ch>
2021-10-22 07:23:45 +0200

    Return single product - Custom Hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import CustomHooks from "./components/CustomHooks/CustomHooks";
+import PropTypes from "./components/PropTypes";
 
 function App() {
 	return (
 		<div className='container'>
 			<h1>Advanced React</h1>
-			<CustomHooks />
+			<PropTypes />
 		</div>
 	);
 }
---
+++ src/components/PropTypes/Product.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Products = () => {
+	return (
+		<div>
+			<article className='product'>single product</article>
+		</div>
+	);
+};
+
+export default Products;

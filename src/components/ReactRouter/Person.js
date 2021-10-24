87af9c9c895820392cb6980ddca8c57e9cb642c3
l33th <l33th@protonmail.ch>
2021-10-24 08:11:42 +0200

    People Link and Back Button

--- src/components/ReactRouter/Person.js
+++ src/components/ReactRouter/Person.js
@@ -3,9 +3,15 @@ import { data } from '../../data';
 import { Link, useParams } from 'react-router-dom';
 
 const Person = () => {
+  const [name, setName] = useState('default-name');
+  const { id } = useParams();
+
   return (
     <div>
-      <h2>person</h2>
+      <h2>{name}</h2>
+      <Link to='/people' className='btn'>
+        Back To People
+      </Link>
     </div>
   );
 };

df23d86ac29a252789a6e51eb044f5d108be38f5
l33th <l33th@protonmail.ch>
2021-10-25 07:01:25 +0200

    wrapped BigList function in a memo method

--- src/components/UseMemo/index.js
+++ src/components/UseMemo/index.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import useFetchHook from '../CustomHooks/useFetchHook';
 
-const index = () => {
-  return <div>Use Memo</div>;
+const url = 'https://course-api.com/javascript-store-products';
+
+// every time props or state changes, component re-renders
+
+const Index = () => {
+  const { products } = useFetchHook(url);
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Count : {count}</h1>
+      <button className='btn' onClick={() => setCount(count + 1)}>
+        click me
+      </button>
+      <BigList products={products} />
+    </>
+  );
 };
 
-export default index;
+const BigList = React.memo(({ products }) => {
+  return (
+    <section className='products'>
+      {products.map((product) => {
+        return <SingleProduct key={product.id} {...product}></SingleProduct>;
+      })}
+    </section>
+  );
+});
+
+const SingleProduct = ({ fields }) => {
+  let { name, price } = fields;
+  price = price / 100;
+  const image = fields.image[0].url;
+
+  return (
+    <article className='product'>
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>${price}</p>
+    </article>
+  );
+};
+
+export default Index;
